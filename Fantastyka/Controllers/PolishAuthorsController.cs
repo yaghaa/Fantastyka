b5357af3b0bd6e9a5874319d71466a12575b6015
aga <za.rogiem@gmail.com>
2016-12-13 21:53:25 +0100

    Fantastyka w wersji w MVC - widoki Index i OFantastyce

---
+++ Fantastyka/Controllers/PolishAuthorsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fantastyka.Controllers
+{
+    public class PolishAuthorsController : Controller
+    {
+        public ActionResult PolishAuthors()
+        {
+            return View();
+        }
+    }
+}
---
+++ Fantastyka/Controllers/SurveyController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fantastyka.Controllers
+{
+    public class SurveyController : Controller
+    {
+        public ActionResult Survey()
+        {
+            return View();
+        }
+    }
+}
